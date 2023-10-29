68dc2989f7129352c9bc0e9520b1450901aeef49
UsingNamespaceNolan <nolan.johnston335@gmail.com>
2023-10-28 21:41:51 -0400

    removed some half-baked attempts at doing stuff

--- Temporal Mirage - Cybernetic Reclaim/BaseClasses/Drops/Scripts/WeaponDrop.gd
+++ Temporal Mirage - Cybernetic Reclaim/BaseClasses/Drops/Scripts/WeaponDrop.gd
@@ -3,10 +3,8 @@ extends Area2D
 # The weapon class that this drop represents
 @export var weapon_class: Script = null
 
-var confirm = false
-
 func _on_body_entered(body: Node):
 	if body is BaseCharacter and body.is_in_group("player"):
-		if body.swap_weapon(weapon_class):
-			body.update_ammo_display()
-			queue_free()  # Remove the weapon drop from the scene
+		body.swap_weapon(weapon_class)
+		body.update_ammo_display()
+		queue_free()  # Remove the weapon drop from the scene
